23eeb5fc15c1129562a0aa5ced1991d40c576ce8
xunjoy <36482777+xunjoy@users.noreply.github.com>
2022-03-20 19:15:07 -0700

    reveal-react

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './css/styles.css'
 import bloop from "./img/BLOOPCENTER.svg"
 import simpleMap from './img/SIMPLEMAP1.svg'
 import detailedMap from './img/DETAILEDMAP.svg'
+import Fade from 'react-reveal/Fade'
 
 function App() {
   return (
     <div className="App">
       <section className = "main">
+      <Fade duration ={5000}>
         <main>
         <div className="main-content">
             <img src={bloop}  alt="" />
@@ -24,23 +26,35 @@ function App() {
             </div>
         </div>
         </main>
+        </Fade>
       </section>
+
     <div className='body-content'>
+    
       <section className="connect">
+      <Fade duration ={5000}>
         <div className="connect-content">
           <h2 className='text-one'>Connect <span className='pale-white' >your <br /> interests</span></h2>
           <img className='simpleMap' src={simpleMap} alt=""/>
         </div>
+        </Fade>
       </section>
+      
 
+      
       <section className="learn">
+      <Fade duration ={5000}>
         <div className="learn-content">
           <h2 className='text-one'> <span className='pale-white'>Learn from your <br /></span>journey  <span className='pale-white'> and see <br /> your internet</span></h2>
           <img className='detailedMap' src={detailedMap} alt=""/>
         </div>
+        </Fade>
       </section>
+      
     </div>
+
       <div className='footer-cta'>
+      <Fade duration ={2000}>
         <div className="cta">
             <form>
               <input type="text" placeholder="What's your email?"/>
@@ -49,6 +63,7 @@ function App() {
             <p className= 'main-text'>Available for all desktop platforms</p>
             <p></p>
         </div>
+      </Fade>
       </div>
       <div className='copyright-cta'>
         <div className= 'copyright-text'>
