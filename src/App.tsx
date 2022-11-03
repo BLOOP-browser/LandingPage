dc24ad8936a49091e13405a5dc5ccde5b0d7fcf5
xunjoy <36482777+xunjoy@users.noreply.github.com>
2022-11-03 13:37:47 +0100

    changed sign-up form and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -81,8 +81,9 @@ function App() {
       <div className='footer-cta'>
         <Fade duration ={2000}>
           <div className="footer-cta-content">
-            <h3 >Get access to the BLOOP Search beta</h3>
-            <SignUpForm/>
+            <a href = "https://tally.so/r/nGe9Yo" id='sign-up-button' > Try the BLOOP beta</a>
+            {/* <h3 >Get access to the BLOOP Search beta</h3> */}
+            {/* <SignUpForm/> */}
           </div>
         </Fade>
       </div>
